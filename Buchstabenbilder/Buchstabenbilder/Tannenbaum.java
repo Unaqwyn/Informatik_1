6031e607bf335e34fa296ccb6833ee3d9a4ba9b3
Romario B. Jäger <jaeger.romario@ntb.ch>
2017-12-12 16:28:09 +0100

    tannenbaum_V2

--- Buchstabenbilder/Buchstabenbilder/Tannenbaum.java
+++ Buchstabenbilder/Buchstabenbilder/Tannenbaum.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 /**
  * Write a description of class Tannenbaum here.
  *
@@ -20,6 +20,7 @@ public class Tannenbaum
 
     public void tannenbaum(int hoehe)
     {
+        Random random = new Random();
         System.out.print('\u000C');
         int breite = 2*hoehe;
         for(int i=0;i<hoehe;i++)
@@ -28,7 +29,18 @@ public class Tannenbaum
             {
                 if(j>=hoehe-i&&j<=hoehe+i)
                 {
-                    System.out.print("*");
+                    if(random.nextInt(10)==1&&i!=0)
+                    {
+                        System.out.print("O");
+                    }
+                    else if(random.nextInt(10)==2&&i!=0)
+                    {
+                        System.out.print("i");
+                    }
+                    else
+                    {
+                        System.out.print("*");
+                    }
                 }
                 else if(j==breite-1)
                 {
@@ -47,5 +59,54 @@ public class Tannenbaum
         }
         System.out.print("I");
         System.out.println();
+    }
+    
+    public void tannenbaum(int hoehe, int stammHoehe, int stammBreite)
+    {
+        Random random = new Random();
+        System.out.print('\u000C');
+        int breite = 2*hoehe;
+        for(int i=0;i<hoehe;i++)
+        {
+            for(int j=0;j<breite;j++)
+            {
+                if(j>=hoehe-i&&j<=hoehe+i)
+                {
+                    if(random.nextInt(10)==1&&i!=0)
+                    {
+                        System.out.print("O");
+                    }
+                    else if(random.nextInt(10)==2&&i!=0)
+                    {
+                        System.out.print("i");
+                    }
+                    else
+                    {
+                        System.out.print("*");
+                    }
+                }
+                else if(j==breite-1)
+                {
+                    System.out.println();
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+        System.out.println();
+        for(int m=0;m<stammHoehe;m++)
+        {
+            for(int k=0;k<(hoehe-(stammBreite/2));k++)
+            {
+                System.out.print(" ");
+            }
+            for(int l=0;l<stammBreite;l++)
+            {
+                System.out.print("I");
+            }
+            System.out.println();
+        }
     }
 }
